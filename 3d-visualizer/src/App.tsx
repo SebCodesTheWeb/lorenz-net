efaba42643d340a3655b047827da889c32ef1520
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-10-26 18:52:21 +0200

    feat(3d-visualizer): add basic attractor field

--- 3d-visualizer/src/App.tsx
+++ 3d-visualizer/src/App.tsx
@@ -1,34 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useMemo, forwardRef } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { OrbitControls, QuadraticBezierLine, Line } from '@react-three/drei'
 import './App.css'
+
+const xParam = 10
+const yParam = 28
+const zParam = 8 / 3
+
+function randomColor() {
+  // Generate a random integer between 0 and 255 for each color component
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  
+  // Convert each component to hexadecimal, ensuring it is two digits
+  const redHex = red.toString(16).padStart(2, '0');
+  const greenHex = green.toString(16).padStart(2, '0');
+  const blueHex = blue.toString(16).padStart(2, '0');
+  
+  // Combine the components into a single hex color string
+  return `#${redHex}${greenHex}${blueHex}`;
+}
+
+
+function Tracer({ targetRef }) {
+  const lineRef = useRef()
+  const [points, setPoints] = useState<number[][]>([[0, 0, 0]])
+  const color = useMemo(() => randomColor(), [])
+
+  useFrame(() => {
+    if (targetRef.current) {
+      setPoints((prev) => [...prev, targetRef.current.position.clone()])
+    }
+  })
+
+  return (
+    <Line
+      points={points}
+      color={color}
+    />
+  )
+}
+
+const Box = forwardRef((props: unknown, ref: any) => {
+  // const ref = useRef<unknown>(null)
+  useFrame((state, delta) => {
+    if (!ref.current) return
+    const { x, y, z } = ref.current.position
+    const xDelta = xParam * (y - x)
+    const yDelta = x * (yParam - z) - y
+    const zDelta = x * y - zParam * z
+
+    ref.current.position.x += (xDelta * delta) / 1
+    ref.current.position.y += (yDelta * delta) / 1
+    ref.current.position.z += (zDelta * delta) / 1
+  })
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  )
+})
+
+function CameraLogger() {
+  const { camera } = useThree()
+
+  useFrame(() => {
+    console.log('Camera Position:', camera.position)
+    console.log('Camera Rotation:', camera.rotation)
+  })
+
+  return null
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const boxRefOne = useRef()
+  const boxRefTwo = useRef()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} 
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas camera={{ position: [-8, 15, -66], fov: 50 }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} ref={boxRefOne} /> 
+      <Box position={[-1.2000001, 0, 0]} ref={boxRefTwo} /> 
+      <Tracer targetRef={boxRefOne} />
+      <Tracer targetRef={boxRefTwo} />
+      <gridHelper args={[500, 1000]} />
+      {/* <CameraLogger /> */}
+      <OrbitControls />
+    </Canvas>
   )
 }
 
