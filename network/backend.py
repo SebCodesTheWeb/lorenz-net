2c6e244a3b96ea424764fbfc84fb90da642d8a00
Sebastian Delgado <sebastian.delgado@mediatool.com>
2024-02-06 18:23:35 +0100

    feat: got initial transformers architecture working

--- network/backend.py
+++ network/backend.py
@@ -6,6 +6,7 @@ from device import device
 from lstm_rnn import LSTM_RNN
 from constants import dt
 from lorenz import RK4, eulers_method
+from transformer import SimpleTransformerModel
 
 app = Flask(__name__)
 CORS(app)
@@ -13,8 +14,12 @@ CORS(app)
 hidden_size = 50
 num_layers = 1
 input_size, output_size = 3, 3
-model = LSTM_RNN(input_size, hidden_size, output_size, num_layers).to(device)
-model.load_state_dict(torch.load('lstm_rnn_lorenz.path'))
+
+rnn_model = LSTM_RNN(input_size, hidden_size, output_size, num_layers).to(device)
+rnn_model.load_state_dict(torch.load('lstm_rnn_lorenz.path'))
+
+transformer_model = SimpleTransformerModel(input_size, output_size, 50, 2, 2).to(device)
+transformer_model.load_state_dict(torch.load('transformer_lorenz.path'))
 
 @app.route("/predict", methods=['GET'])
 def predict_path():
@@ -28,11 +33,34 @@ def predict_path():
 
     with torch.no_grad():
         for _ in range(num_steps):
-            next_pos = model(current_pos_tensor).cpu().numpy()[0].tolist()
+            next_pos = rnn_model(current_pos_tensor).cpu().numpy()[0].tolist()
             path.append(next_pos)
 
             current_pos_tensor = torch.tensor([next_pos], dtype=torch.float32).to(device).unsqueeze(0)
 
+    return jsonify(path)
+
+
+@app.route("/predict_w_transformer", methods=['GET'])
+def transformer_predict_path():
+    print('ran')
+    t = float(request.args.get('t'))
+    init_pos = request.args.get('init_pos').split(',')
+    init_pos = [float(x) for x in init_pos]
+    current_pos_tensor = torch.tensor([init_pos], dtype=torch.float32).to(device)
+
+    num_steps = int(t / dt)
+    path = [init_pos]
+
+    with torch.no_grad():
+        for _ in range(num_steps):
+            next_pos = transformer_model(current_pos_tensor).cpu().numpy()[0].tolist()
+            next_pos = [item for sublist in next_pos for item in sublist]  # Flatten the list
+            path.append(next_pos)
+
+            current_pos_tensor = torch.tensor([next_pos], dtype=torch.float32).to(device)
+
+    print(path)
     return jsonify(path)
 
 @app.route("/rk4_predict", methods=['GET'])
