4cf7f37b8b0c5213de6cece184d06c408ce03c8d
Sebastian Delgado <sebastian.delgado@mediatool.com>
2024-03-02 15:27:28 +0100

    implemented new transformer

--- network/constants.py
+++ network/constants.py
@@ -3,4 +3,4 @@ chunk_len = 2000
 dt = 0.05
 test_ratio = 0.01
 #How many values to take as input before predicting the next one
-inp_seq_len = 50 
+inp_seq_len = 1
--- network/train_transformer.py
+++ network/train_transformer.py
@@ -1,29 +1,32 @@
-from get_training_data import x_train, y_train
-from transformer import SimpleTransformerModel
+from get_transformer_training_data import x_train, y_train
+from transformer import TransformerModel
 from torch import nn
 from device import device
 from torch.utils.data import DataLoader, TensorDataset
 import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR
 import torch
 
-print(x_train, y_train)
-
 # Hyperparams
-hidden_dim = 50
+hidden_dim = 500
 nhead = 2
 num_layers = 2
 learning_rate = 0.0005
-batch_size = 100
+batch_size = 64
+#Currently does not actually care
+vocab_size = 3
+# d_model has to be even due to how positional encoding is implemented, requiring pairs of sin and cosine positions
+d_model = 128
+dropout = 0.1
 
 train_data = TensorDataset(x_train, y_train)
 train_dataloader = DataLoader(train_data, batch_size=batch_size)
 
-input_size = x_train.shape[2]  
-output_size = y_train.shape[2]
-model = SimpleTransformerModel(input_size, output_size, hidden_dim, nhead, num_layers).to(device)
+model = TransformerModel(ntoken=vocab_size, d_model=d_model, nhead=nhead, d_hid=hidden_dim, nlayers=num_layers,dropout=dropout).to(device)
 
 loss_fn = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = ExponentialLR(optimizer, gamma=0.9)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -32,8 +35,13 @@ def train(dataloader, model, loss_fn, optimizer):
     running_loss = 0.0
 
     for batch_nbr, (seq, label) in enumerate(dataloader):
-        seq, label = seq.to(device), label.to(device)
+        seq , label = seq.to(device), label.to(device)
         prediction = model(seq)
+        # print(f"Prediction shape: {prediction.shape}, Label shape: {label.shape}")  
+        # Prediction shape: torch.Size([batch_size=100, seq_len=50, feature_size=3]), Label shape: torch.Size([batch_size, feature_size])
+        prediction = prediction[:, -1, :] # Selects the last point the in predicted sequence
+
+
         loss = loss_fn(prediction, label)
 
         optimizer.zero_grad()
@@ -48,8 +56,9 @@ def train(dataloader, model, loss_fn, optimizer):
     running_loss /= num_batches
     print(f"Average loss for epoch: {running_loss:>7f}")
 
-epochs = 5
+epochs = 10
 for t in range(epochs):
     train(train_dataloader, model, loss_fn, optimizer)
+    scheduler.step()
 print("Done")
 torch.save(model.state_dict(), 'transformer_lorenz.path')
