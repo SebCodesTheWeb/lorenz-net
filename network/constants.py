4c7244de986e36d63b0b3ad744266796347eccc6
Sebastian Delgado <sebastian.delgado@mediatool.com>
2024-03-10 22:53:23 +0100

    transformer-test-data-2

--- network/constants.py
+++ network/constants.py
@@ -1,6 +1,6 @@
 seed_nbr = 0
-chunk_len = 50
-dt = 0.03
+chunk_len = 10
+dt = 0.04
 test_ratio = 0.01
 val_ratio = 0.15
 #How many values to take as input before predicting the next one, (should be one less than chunk_len)
