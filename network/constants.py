d06f60a43f5c28a7ab30cbf84af659a6256b4aac
“Sebastian” <zenrigod@gmail.com>
2024-03-11 10:05:14 +0000

    test-two

--- network/constants.py
+++ network/constants.py
@@ -1,6 +1,6 @@
 seed_nbr = 0
 chunk_len = 100
-dt = 0.07
+dt = 0.04
 test_ratio = 0.01
 val_ratio = 0.15
 #How many values to take as input before predicting the next one, (should be one less than chunk_len)
